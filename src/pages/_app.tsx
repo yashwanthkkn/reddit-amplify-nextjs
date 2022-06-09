ed8a685fdf53b0f760b2748e993dababaefa9b39
Yashwanth K <yashwanthkkn@gmail.com>
2022-06-09 12:46:26 +0530

    inti commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import Head from "next/head";
+import PropTypes from "prop-types";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider } from "@emotion/react";
+import theme from "../../theme";
+import createEmotionCache from "../../createEmotionCache";
+
+const clientSideEmotionCache = createEmotionCache();
+
+export default function MyApp(props) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    // <CacheProvider value={emotionCache}>
+    <React.Fragment>
+      <Head>
+        <title>Reddit</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </React.Fragment>
+    // </CacheProvider>
+  );
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  emotionCache: PropTypes.object,
+  pageProps: PropTypes.object.isRequired,
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,4 @@
+import { Typography } from "@mui/material";
+export default function Home() {
+  return <Typography variant="h1">Hello</Typography>;
+}
